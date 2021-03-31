046c42a32386bb4e8233cc135ef9b1f6a6a740ac
thesmartduck <kayastha.netra23@gmail.com>
2021-03-30 21:06:07 -0400

    Update main.tf

--- AWS_EC2_intance/main.tf
+++ AWS_EC2_intance/main.tf
@@ -1,1 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.32"
+    }
+  }
+}
 
+provider "aws" {
+  profile = # user profile
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-00029868326438a09"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+    Type = "Test"
+  }
+
+  hibernation = true
+
+  root_block_device {
+    encrypted = true
+  }
+}
