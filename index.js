7ac41ebd368bf5d6f0a924307152f27b641db721
thesmartduck <kayastha.netra23@gmail.com>
2021-03-30 20:58:42 -0400

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-// 'Hello World' nodejs12.x runtime AWS Lambda function
-
-
-exports.handler = function(event, context) {
-    console.log('Hello World!');
-    context.succeed('Hello, World!');
-   };
